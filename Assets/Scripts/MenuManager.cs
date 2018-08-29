eb5414969bacb8d39d78fc4ff99c6d4912750646
Saul Aceves Montes <54ulxd@gmail.com>
2018-08-29 00:07:10 -0500

    a hora si commit final xd
    se le agrego un menu sencillo

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+
+public class MenuManager: MonoBehaviour {
+
+  
+
+
+    public void loadlevel(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+
+
+
+    
+}
